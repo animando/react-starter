f0926e853ac46746bd2c43b6f7855d6aae91245b
Graham King <gking2224@gmail.com>
2020-11-13 20:38:30 +0000

    Hooks

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,61 +4,36 @@ import { getValue } from './store/counter/counterSelectors';
 import { Button } from './components';
 import { decrement, increment } from './store/counter/counterReducer';
 
-class CounterView extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showButtons: false,
-    };
-    this.onShowButtons = this.onShowButtons.bind(this);
-    this.onHideButtons = this.onHideButtons.bind(this);
-  }
+const CounterView = ({ value, onIncrement, onDecrement }) => {
+  const [showButtons, setShowButtons] = React.useState(false);
 
-  onShowButtons() {
-    this.setState({
-      showButtons: true,
-    });
-  }
+  const onHideButtons = React.useCallback(() => {
+    setShowButtons(false);
+  }, []);
+  const onShowButtons = React.useCallback(() => {
+    setShowButtons(true);
+  }, []);
 
-  onHideButtons() {
-    this.setState({
-      showButtons: false,
-    });
-  }
-
-  render() {
-    const { value, onIncrement, onDecrement } = this.props;
-    const { showButtons } = this.state;
-
-    return (
+  return (
+    <div>
+      Value: {value}
+      {showButtons && (
+      <>
+        <Button onClick={onIncrement} text="Increment" />
+        <Button onClick={onDecrement} text="Decrement" />
+        <div>
+          <Button onClick={onHideButtons} text="Hide Buttons" />
+        </div>
+      </>
+      )}
+      {!showButtons && (
       <div>
-        Value: {value}
-        {showButtons && (
-          <>
-            <Button onClick={onIncrement} text="Increment" />
-            <Button onClick={onDecrement} text="Decrement" />
-            <div>
-              <Button onClick={this.onHideButtons} text="Hide" />
-            </div>
-          </>
-        )}
-        {!showButtons && (
-          <div>
-            <Button onClick={this.onShowButtons} text="Show Buttons" />
-          </div>
-        )}
+        <Button onClick={onShowButtons} text="Show Buttons" />
       </div>
-    );
-  }
-}
-
-// const CounterView = ({ value, onIncrement, onDecrement }) => (
-//       <div>
-//         Value: {value}
-//         <Button onClick={onIncrement} text="Increment" />
-//         <Button onClick={onDecrement} text="Decrement" />
-//       </div>
-//     )
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   value: getValue(state),
