1fb0f96b5602c8ed2ea7151e49f331a87272668b
Graham King <gking2224@gmail.com>
2020-11-13 20:24:50 +0000

    Added internal state

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,13 +4,61 @@ import { getValue } from './store/counter/counterSelectors';
 import { Button } from './components';
 import { decrement, increment } from './store/counter/counterReducer';
 
-const CounterView = ({ value, onIncrement, onDecrement }) => (
-  <div>
-    Value: {value}
-    <Button onClick={onIncrement} text="Increment" />
-    <Button onClick={onDecrement} text="Decrement" />
-  </div>
-);
+class CounterView extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showButtons: false,
+    };
+    this.onShowButtons = this.onShowButtons.bind(this);
+    this.onHideButtons = this.onHideButtons.bind(this);
+  }
+
+  onShowButtons() {
+    this.setState({
+      showButtons: true,
+    });
+  }
+
+  onHideButtons() {
+    this.setState({
+      showButtons: false,
+    });
+  }
+
+  render() {
+    const { value, onIncrement, onDecrement } = this.props;
+    const { showButtons } = this.state;
+
+    return (
+      <div>
+        Value: {value}
+        {showButtons && (
+          <>
+            <Button onClick={onIncrement} text="Increment" />
+            <Button onClick={onDecrement} text="Decrement" />
+            <div>
+              <Button onClick={this.onHideButtons} text="Hide" />
+            </div>
+          </>
+        )}
+        {!showButtons && (
+          <div>
+            <Button onClick={this.onShowButtons} text="Show Buttons" />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+// const CounterView = ({ value, onIncrement, onDecrement }) => (
+//       <div>
+//         Value: {value}
+//         <Button onClick={onIncrement} text="Increment" />
+//         <Button onClick={onDecrement} text="Decrement" />
+//       </div>
+//     )
 
 const mapStateToProps = (state) => ({
   value: getValue(state),
